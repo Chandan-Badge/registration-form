0e0ba2aae6109030ba91f23caa8b5b5f87e42066
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-27 11:14:13 +0530

    Improvement 05

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-let btn = document.querySelector("#sub-btn");
+let btn = document.querySelector(".sub-btn");
 let inpArea = document.querySelector(".input-area");
-
-let newPop = document.createElement("p");
-newPop.innerText = "Form was Submited... 👍";
-
-let mainBox = document.querySelector(".main-box");
+const popup = document.querySelector(".popup");
 
 btn.addEventListener("click", () => {
-
-    // alert("Form was Registerd.");
-    inpArea.remove();
-    btn.remove();
-
-    newPop.classList.add("newPopStyle");
-    mainBox.appendChild(newPop);
+    popup.classList.add("open-popup");
 })
