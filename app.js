036a9d06a17c512d16f72ae9af14bbcd1b4c652d
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-27 11:16:48 +0530

    Improvement 05

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 let btn = document.querySelector(".sub-btn");
 let inpArea = document.querySelector(".input-area");
 const popup = document.querySelector(".popup");
+let okbtn = document.querySelector(".okbtn");
 
 btn.addEventListener("click", () => {
     popup.classList.add("open-popup");
+})
+
+okbtn.addEventListener("click", () => {
+    popup.classList.remove("open-popup");
 })
